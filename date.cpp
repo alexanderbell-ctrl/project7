c5ed0df7c6347d0fd7f36a844a21cdd364218bbf
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-02-20 20:34:10 +0000

    date 'works' with blank constructor, not using init

---
+++ date.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+
+#include "date.h"
+
+Date::Date(){
+	month = 11;
+	day = 11;
+	year = 1111;
+} //end constructor
+ 
+void Date::init(std::string dateString){
+	std::string stringMonth;
+	std::string stringDay;
+	std::string stringYear;
+	std::stringstream converter;
+
+	converter.str(dateString);
+	getline(converter, stringMonth, '/');
+	getline(converter, stringDay, '/');
+	getline(converter, stringYear);
+
+	converter << stringMonth << stringDay << stringYear;
+	converter >> month >> day >> year;
+} //end init
+
+void Date::printDate(){
+	std::cout << month << " " << day << ", " << year << std::endl;
+} //end printDate
--- makefile
+++ makefile
@@ -1,11 +1,14 @@
-students: main.o address.o
-	g++ main.o address.o -o students
+students: main.o address.o date.o
+	g++ main.o address.o date.o -o students
 
-main.o: main.cpp address.h
+main.o: main.cpp address.h date.h
 	g++ -c main.cpp 
 
 address.o: address.h address.cpp
 	g++ -c address.cpp
+
+date.o: date.h date.cpp
+	g++ -c date.cpp
 
 clean: 
 	rm students
